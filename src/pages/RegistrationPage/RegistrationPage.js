059e29b6526bd87f93faa53d7917f7acf6203212
Ihor Volovyk <ihor.volovyk@gmail.com>
2019-10-29 14:04:23 +0200

    version 1

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import css from './RegistrationPage.module.css';
 
 class RegistrationPage extends Component {
-  state = {};
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
 
   render() {
-    return <h2>RegistrationPage</h2>;
+    return (
+      <section className={css.reg_section}>
+        <div className={css.container}>
+          <div className={css.reg_form}>
+            <form>
+              <a href="google">Google</a>
+            </form>
+          </div>
+          <div className={css.reg_text}>12</div>
+        </div>
+      </section>
+    );
   }
 }
 
