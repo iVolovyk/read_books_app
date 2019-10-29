e9792a4d07ee004dc4176e9738384e82afdc62c5
Kalysh <503292@ukr.net>
2019-10-29 13:56:41 +0200

    ADD _ mobile styles finish

--- src/components/BooksList/TemplateToBooks/TemplateToBooks.js
+++ src/components/BooksList/TemplateToBooks/TemplateToBooks.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import menuBook from '../../../assets/icons/library/menu_book-24px.svg';
 import css from './TemplateToBooks.module.css';
 import { ReactComponent as LibLogo } from '../../../assets/icons/library/menu_book-24px.svg';
 
@@ -7,67 +6,45 @@ const TemplateToBooks = list => {
   // console.log(list.list.listBooks);
   return (
     <>
-      <hr />
-
-      {/* <ul className={css.ulBooks}>
-        {list.list.listBooks.map(el => (
-          <li className={css.liBooks} key={el.id}>
-            <div className={css.wrapHead}>
-              <div className={css.nameHead}>Назва</div>
-              <div className={css.authorHead}>Автор</div>
-              <div className={css.yearHead}>Рік</div>
-              <div className={css.pageHead}>Стор.</div>
-              <div className={css.ratingHead}>Рейтинг</div>
-            </div>
-            <div className={css.wrapBody}>
-              <div className={css.nameBody}>
-                <LibLogo className={css.libLogo} />
-                {el.name}
-              </div>
-              <div className={css.authorBody}>{el.author}</div>
-              <div className={css.yearBody}>{el.year}</div>
-              <div className={css.pageBody}>{el.page}</div>
-              {el.rating !== undefined && (
-                <>
-                  <div className={css.ratingBody}>{el.rating}</div>
-                  <div className={css.ratingIcon}>ICON</div>
-                </>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul> */}
       <ul className={css.ulBooks}>
         {list.list.listBooks.map(el => (
           <li className={css.liBooks} key={el.id}>
             <div className={css.wrapName}>
-              <div className={css.nameHead}>Назва</div>
-              <div className={css.nameBody}>
-                <LibLogo className={css.libLogo} />
-                {el.name}
+              <LibLogo className={css.libLogo} />
+              <div className={`${css.nameHead} ${css.title}`}>
+                Назва книги<span>:</span>
               </div>
+              <div className={css.nameBody}>{el.name}</div>
             </div>
             <div className={css.wrapAuthor}>
-              <div className={css.authorHead}>Автор</div>
+              <div className={`${css.authorHead} ${css.title}`}>
+                Автор<span>:</span>
+              </div>
               <div className={css.authorBody}>{el.author}</div>
             </div>
             <div className={css.wrapYear}>
-              <div className={css.yearHead}>Рік</div>
+              <div className={`${css.yearHead} ${css.title}`}>
+                Рік<span>:</span>
+              </div>
               <div className={css.yearBody}>{el.year}</div>
             </div>
             <div className={css.wrapPage}>
-              <div className={css.pageHead}>Стор.</div>
+              <div className={`${css.pageHead} ${css.title}`}>
+                Стор.<span>:</span>
+              </div>
               <div className={css.pageBody}>{el.page}</div>
             </div>
             <div className={css.wrapRating}>
-              <div className={css.ratingHead}>Рейтинг</div>
+              <div className={`${css.ratingHead} ${css.title}`}>
+                Рейтинг<span>:</span>
+              </div>
               {el.rating !== undefined && (
                 <>
                   <div className={css.ratingBody}>{el.rating}</div>
-                  <div className={css.ratingIcon}>ICON</div>
                 </>
               )}
             </div>
+            <div className={css.ratingIcon}>ICON</div>
           </li>
         ))}
       </ul>
