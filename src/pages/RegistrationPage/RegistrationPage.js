e13decdce3fbf4f2200ba008a25eec36f5d2ec32
Ihor Volovyk <ihor.volovyk@gmail.com>
2019-10-29 17:44:38 +0200

    last

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -9,17 +9,72 @@ class RegistrationPage extends Component {
     confirmPassword: '',
   };
 
+  // handleChange = () => {
+  //   const {} = e.target.name;
+  // };
+
   render() {
+    const { name, email, password, confirmPassword } = this.state;
     return (
       <section className={css.reg_section}>
-        <div className={css.container}>
-          <div className={css.reg_form}>
-            <form>
-              <a href="google">Google</a>
-            </form>
-          </div>
-          <div className={css.reg_text}>12</div>
+        <div className={css.reg_form}>
+          <form>
+            <div className={css.reg_social}>
+              <a
+                className={css.reg_social_button}
+                href="https://book-read.goit.co.ua/api/v1/auth/google"
+              >
+                Google
+              </a>
+            </div>
+            <label htmlFor="name">
+              Ім`я
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                placeholder="Ім'я"
+                required="required"
+              />
+            </label>
+            <label label htmlFor="email">
+              Електронна адреса
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                placeholder="your@mail.com"
+                required="required"
+              />
+            </label>
+            <label label htmlFor="password">
+              Пароль
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                placeholder="Пароль"
+                required="required"
+              />
+            </label>
+            <label label htmlFor="confirmPassword">
+              Підтвердити пароль
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={confirmPassword}
+                placeholder="Підтвердити пароль"
+                required="required"
+              />
+            </label>
+            <button type="submit">Зареєструватися</button>
+          </form>
         </div>
+        <div className={css.reg_text}>12</div>
       </section>
     );
   }
