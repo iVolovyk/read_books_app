c522297a5083994f1730042483bd6f397bdde392
Kalysh <503292@ukr.net>
2019-10-30 11:15:50 +0200

    REFACT _ structure / TODO _ styled

--- src/components/BooksList/TemplateToBooks/TemplateToBooks.js
+++ src/components/BooksList/TemplateToBooks/TemplateToBooks.js
@@ -6,40 +6,36 @@ import { ReactComponent as PenLogo } from '../../../assets/icons/pen/create-24px
 
 const TemplateToBooks = list => {
   // console.log(list.list.listBooks);
+  const windowWidth = document.documentElement.clientWidth;
   return (
     <>
+      {windowWidth >= 767 && (
+        <div>
+          <div className={`${css.nameHead} ${css.title}`}>Назва книги</div>
+          <div className={`${css.authorHead} ${css.title}`}>Автор</div>
+          <div className={`${css.yearHead} ${css.title}`}>Рік</div>
+          <div className={`${css.pageHead} ${css.title}`}>Сторінок</div>
+          <div className={`${css.ratingHead} ${css.title}`}>Рейтинг</div>
+        </div>
+      )}
       <ul className={css.ulBooks}>
         {list.list.listBooks.map(el => (
           <li className={css.liBooks} key={el.id}>
             <div className={css.wrapName}>
               <LibLogo className={css.libLogo} />
-              <div className={`${css.nameHead} ${css.title}`}>
-                Назва книги<span>:</span>
-              </div>
+
               <div className={css.nameBody}>{el.name}</div>
             </div>
             <div className={css.wrapAuthor}>
-              <div className={`${css.authorHead} ${css.title}`}>
-                Автор<span>:</span>
-              </div>
               <div className={css.authorBody}>{el.author}</div>
             </div>
             <div className={css.wrapYear}>
-              <div className={`${css.yearHead} ${css.title}`}>
-                Рік<span>:</span>
-              </div>
               <div className={css.yearBody}>{el.year}</div>
             </div>
             <div className={css.wrapPage}>
-              <div className={`${css.pageHead} ${css.title}`}>
-                Сторінок<span>:</span>
-              </div>
               <div className={css.pageBody}>{el.page}</div>
             </div>
             <div className={css.wrapRating}>
-              <div className={`${css.ratingHead} ${css.title}`}>
-                Рейтинг<span>:</span>
-              </div>
               {/* {el.rating !== undefined && (
                 <> */}
               <div className={css.ratingBody}>
