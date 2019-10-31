e10da1a25bc4fb099d04cf280a29475b30fe3919
Denze1l <dvornikovd.o.1998@gmail.com>
2019-10-31 17:02:08 +0200

    datapicker

--- src/components/CreateWorkout/CreateWorkout.js
+++ src/components/CreateWorkout/CreateWorkout.js
@@ -3,18 +3,46 @@ import Datetime from 'react-datetime';
 import style from './style.module.css';
 import WorkSelect from './CreateSelect/Select';
 import ChosenBooks from './ChoosenBooks/ChoosenBooks';
+import 'react-datetime/css/react-datetime.css';
 
 class CreateWorkout extends Component {
   state = {
     books: [],
+    todayDate: null,
+    ChosenDate: null,
+    totalPages: null,
+    totalBooks: null,
+  };
+
+  componentDidUpdate() {
+    console.log(this.state);
+    console.log(this.state);
+  }
+
+  dateOnchangeMethod = date => {
+    this.setState({
+      todayDate: Datetime.moment().format('MMM Do YY'),
+      ChosenDate: date.format('MMM Do YY'),
+    });
   };
 
   render() {
     const { books } = this.state;
+    const yesterday = Datetime.moment().subtract(1, 'day');
+    const valid = current => {
+      return current.isAfter(yesterday);
+    };
     return (
       <section className={style.Workout}>
         <h2 className={style.headline}>Моє тренування</h2>
-        <Datetime placeholder="завершення" className={style.dataPeacker} />
+        <Datetime
+          isValidDate={valid}
+          className={style.dataPeacker}
+          onChange={this.dateOnchangeMethod}
+          defaultValue="Я справлюсь за"
+          locale="uk"
+          dateFormat="DD.MM.YYYY"
+        />
         <div className={style.bookChooser}>
           <WorkSelect className={style.select} />
           <button className={style.addButt} type="button">
