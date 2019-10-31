84105bb55a3149fce8074f2ae6b48ae716216eaa
Kalysh <503292@ukr.net>
2019-10-31 15:29:53 +0200

    ADD _ in booksSelectors methods /TO-DO _ button for SummaryModal

--- src/components/BooksList/BoolksListContainer.js
+++ src/components/BooksList/BoolksListContainer.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
 import BooksList from './BooksList';
+// import { getPlaneReadBooks } from '../../redux/books/booksSelectors';git
 
 const mapStateToProps = state => ({
-  readBooks: state,
-  nowReadBooks: state,
-  planeReadBooks: state,
+  // readBooks: getReadBooks(state),
+  // nowReadBooks: getNowReadBooks(state),
+  // planeReadBooks: getPlaneReadBooks(state),
 });
 // TODO  get method for store state in Section
 
