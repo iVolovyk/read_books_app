709ee950c76efccb626143e75e6e546d749ca6cb
Kalysh <503292@ukr.net>
2019-11-01 12:02:28 +0200

    ADD _ in logoPen button / ADD _ open in logoPen-button SummaryModal and close SummaryModal / DO _ change for store.componentController.summeryModalOpen false to true

--- src/redux/controls/controlsActions.js
+++ src/redux/controls/controlsActions.js
@@ -1,5 +1,14 @@
 export const Type = {
-  // EXAMPLE: 'EXAMPLE',
+  SET_SUMMARY_MODAL_ON: 'SET_SUMMARY_MODAL_ON',
+  SET_SUMMARY_MODAL_OFF: 'SET_SUMMARY_MODAL_OFF',
 };
 
-export const exampleAction = () => {};
+export const setSummaryModalOn = value => ({
+  type: Type.SET_SUMMARY_MODAL_ON,
+  payload: value,
+});
+
+export const setSummaryModalOFF = () => ({
+  type: Type.SET_SUMMARY_MODAL_OFF,
+  payload: false,
+});
