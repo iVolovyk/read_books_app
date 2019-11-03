af41ccf3ef73dec6123f4f1b19a6537197e8cd98
arteboris <arteboris@ukr.net>
2019-11-03 11:17:38 +0200

    save for merge

---
+++ src/components/ModalCongrats/ModalCongrats.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './ModalCongrats.module.css';
+
+class ModalCongrats extends Component {
+  state = { on: false };
+
+  componentDidMount() {
+    const { isOpen } = this.props;
+    window.addEventListener('keyup', this.handleKeyPress);
+    this.setState({ on: isOpen });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { isOpen } = this.props;
+    if (prevProps.isOpen !== isOpen) {
+      this.openProps(isOpen);
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keyup', this.handleKeyPress);
+  }
+
+  openProps = isOpen => {
+    this.setState({ on: isOpen });
+  };
+
+  handleKeyPress = e => {
+    const keyCode = e.keyCode || e.which;
+    if (keyCode === 27) {
+      this.setState({ on: false });
+      e.preventDefault();
+    }
+  };
+
+  handleModalCongratsClick = ({ target, currentTarget }) => {
+    if (currentTarget && target !== currentTarget) {
+      return;
+    }
+
+    this.open();
+  };
+
+  open = () => {
+    const { on } = this.state;
+    this.setState({ on: !on });
+  };
+
+  render() {
+    const { children } = this.props;
+    const { on } = this.state;
+
+    return (
+      <>
+        {on && (
+          <div
+            role="toolbar"
+            aria-label="Закрыть"
+            tabIndex={-1}
+            className={css.modal_overlay}
+            onClick={this.handleModalCongratsClick}
+            onKeyUp={this.handleKeyPress}
+          >
+            <div className={css.modal}>
+              {children({
+                isOn: on,
+                onClose: this.open,
+              })}
+            </div>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+ModalCongrats.propTypes = {
+  children: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+};
+
+export default ModalCongrats;
