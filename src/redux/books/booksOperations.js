3b0801bd251b468fd5236260436a38c94c5b8788
arteboris <arteboris@ukr.net>
2019-11-04 22:36:25 +0200

    done AddBook - loader and toast

--- src/redux/books/booksOperations.js
+++ src/redux/books/booksOperations.js
@@ -1,6 +1,13 @@
+import { toast } from 'react-toastify';
 import { addBookStart, addBookSuccess, addBookError } from './booksActions';
 import { addBookOnServer } from '../../services/api';
 
+toast.configure({
+  autoClose: 5000,
+  draggable: false,
+});
+
+// Запускаем асинхронную функцию
 // eslint-disable-next-line import/prefer-default-export
 export const addBook = book => (dispatch, getStore) => {
   const { token } = getStore().session;
@@ -10,6 +17,13 @@ export const addBook = book => (dispatch, getStore) => {
       dispatch(addBookSuccess(response.data));
     })
     .catch(error => {
-      dispatch(addBookError(error));
+      toast.error(
+        'Сталася помилка. Сервіс тимчасово недоступний. Спробуйте, будь-ласка, пізніше',
+        {
+          position: toast.POSITION.TOP_CENTER,
+          className: 'foo-bar',
+        },
+      );
+      return dispatch(addBookError(error));
     });
 };
