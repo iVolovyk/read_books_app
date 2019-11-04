b39714795b1da445cccc687111d8cfea9f11d6c2
taras <stvstudio.com.ua@gmail.com>
2019-11-04 16:46:33 +0200

    ff

---
+++ src/components/Result/Result.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+import ResultForm from "../ResultForm/ResultFormConteiner";
+import css from "./Result.module.css";
+
+function dynamicSort(property) {
+  let sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function(a, b) {
+    /* next line works with strings and numbers,
+     * and you may want to customize it to your needs
+     */
+    let result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result * sortOrder;
+  };
+}
+
+const Result = ({ result }) => {
+  const sortResult = [...result].sort((a, b) =>
+    a.date > b.date ? 1 : b.date > a.date ? -1 : 0
+  );
+
+  return (
+    <div className={css.result}>
+      <ResultForm />
+      <div className={css.resTables}>
+        <h3 className={css.result_title}>статистика</h3>
+        <div className={css.tableWriper}>
+          <table className={css.tableresult}>
+            <tbody>
+              {sortResult.map(row => (
+                <tr key={row._id}>
+                  <td className={css.resData}>
+                    {moment(row.date).format("DD.MM.YYYY")}
+                  </td>
+                  <td className={css.resTime}>
+                    {moment(row.date).format("HH:mm:ss")}
+                  </td>
+                  <td className={css.colstor}>
+                    {row.count}
+                    <span className={css.sporin}>стор.</span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Result.defaultProps = {
+  result: []
+};
+
+Result.propTypes = {
+  result: PropTypes.arrayOf(
+    PropTypes.shape({
+      data: PropTypes.string,
+      time: PropTypes.string,
+      peges: PropTypes.number
+    })
+  )
+};
+
+export default Result;
