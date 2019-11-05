7ce0cd4241e9145223d9ea2a5349abf249413cec
Ihor Volovyk <ihor.volovyk@gmail.com>
2019-11-05 23:30:59 +0200

    recovery 2

--- src/redux/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-const logger = store => next => action => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  console.groupEnd(action.type);
-
-  next(action);
-};
-
-export default logger;
