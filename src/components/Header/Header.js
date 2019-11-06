b0420d88f6b6f286d8a84b844e5c2f364b7210ba
oikuman <oikuman@gmail.com>
2019-11-06 14:15:24 +0200

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,77 +1,130 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
 import css from './Header.module.css';
-import house from '../../assets/icons/home/home-24px.svg';
-import book from '../../assets/icons/library/menu_book-24px.svg';
 import ModalLogoutContainer from '../ModalLogout/ModalLogoutContainer';
-// import Backdrop from '../Backdrop/Backdrop';
+import { logo, menu } from './headerElements/elements';
 
-const Header = ({ name, logout, setLogout, isAuthenticated }) => {
-  const photo = null;
-  const photoPresent = photo || 'm';
-  // const isOpen = false;
-  let modal = null;
-  if (logout) modal = <ModalLogoutContainer />;
-  return (
-    <div className="container">
-      <div className={css.header}>
-        <div className={css.group}>
-          <NavLink to="/library" className={css.logo}>
-            br
-          </NavLink>
-        </div>
+class Header extends Component {
+  state = { deviceWidth: 0 };
 
-        {isAuthenticated && (
-          <>
-            <div className={css.group}>
-              <div className={css.mailLink}>{photoPresent}</div>
-              <p className={css.user}>{name}</p>
-            </div>
-            <div className={css.group}>
-              <NavLink
-                to="/library"
-                className={css.linkButton}
-                activeClassName="active-link"
-              >
-                <img src={book} alt="book" className={css.image} />
-              </NavLink>
-              <NavLink
-                to="/training"
-                className={css.linkButton}
-                activeClassName="active-link"
-              >
-                <img src={house} alt="house" className={css.image} />
-              </NavLink>
+  componentDidMount() {
+    this.setState({ deviceWidth: document.documentElement.clientWidth });
+  }
 
-              <button
-                type="button"
-                className={css.button}
-                onClick={() => setLogout(true)}
-              >
-                Вихід
-              </button>
+  render() {
+    const { logout, setLogout, isAuthenticated, user } = this.props;
 
-              {modal}
-            </div>
-          </>
-        )}
+    let fullname = '';
+    let src = '';
+
+    if (user) {
+      fullname = user.name.fullName;
+      src = user.photo;
+    }
+
+    const letter = <div className={css.empty}>{fullname[0]}</div>;
+    const img = (
+      <div className={css.photo}>
+        <img src={src} alt="user" width="33" height="33" />
       </div>
-    </div>
-  );
+    );
+    let photo = src ? img : letter;
+
+    let name = <p className={css.user}>{fullname}</p>;
+
+    let modal = null;
+    if (logout) modal = <ModalLogoutContainer />;
+
+    const tabletWidth = 768;
+    const { deviceWidth } = this.state;
+    const mobile = deviceWidth < tabletWidth;
+
+    const authentified = isAuthenticated;
+
+    const exit = (
+      <button
+        type="button"
+        className={css.button}
+        onClick={() => setLogout(true)}
+      >
+        Вихід
+      </button>
+    );
+
+    photo = authentified ? photo : null;
+    name = authentified ? name : null;
+    const menuObj = authentified ? menu : null;
+    const exitObj = authentified ? exit : null;
+
+    const groupTwo = mobile ? (
+      <div className={css.group}>
+        {menuObj}
+        {photo}
+        {exitObj}
+      </div>
+    ) : (
+      <div className={css.group}>
+        {photo}
+        {name}
+      </div>
+    );
+
+    const groupThree = mobile ? null : (
+      <div className={css.group}>
+        {menuObj}
+        {exitObj}
+      </div>
+    );
+
+    const content =
+      mobile && !authentified ? (
+        <div className={css.content} style={{ justifyContent: 'center' }}>
+          {logo}
+        </div>
+      ) : (
+        <div className={css.content}>
+          {logo}
+          {groupTwo}
+          {groupThree}
+        </div>
+      );
+
+    return (
+      <div className={css.header}>
+        <div className="container">
+          {content}
+          {modal}
+        </div>
+      </div>
+    );
+  }
+}
+
+Header.defaultProps = {
+  user: PropTypes.shape({
+    haveTraining: false,
+    photo: '',
+    name: PropTypes.shape({
+      firstName: '',
+      lastName: '',
+    }),
+  }),
 };
 
 Header.propTypes = {
-  name: PropTypes.string.isRequired,
   logout: PropTypes.bool.isRequired,
   setLogout: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+    haveTraining: PropTypes.bool,
+    photo: PropTypes.string,
+    name: PropTypes.shape({
+      firstName: PropTypes.string,
+      fullName: PropTypes.string.isRequired,
+      lastName: PropTypes.string,
+    }),
+  }),
 };
 
 export default Header;
-
-Header.propTypes = {
-  name: PropTypes.string.isRequired,
-  logout: PropTypes.bool.isRequired,
-  setLogout: PropTypes.func.isRequired,
-};
